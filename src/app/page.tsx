cd09456b4382fcd67f63cb2453c6498f628a8d9f
zachdressing <zed_dressel@hotmail.com>
2024-04-02 09:24:03 -0700

    finalized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import logoComponent from "./components/logoComponent";
 import dollarIconComponent from "./components/dollarIconComponent";
 import personIconComponent from "./components/personIconComponent";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [billVal, setBillVal] = useState<number>(0)
@@ -12,7 +12,6 @@ export default function Home() {
   const [pplInputVal, setPplInputVal] = useState<string>('');
   const [tipAmount, setTipAmount] = useState<number>(0)
   const [totalAmount, setTotalAmount] = useState<number>(0)
-  let value: number;
 
   useEffect(() => {
     if (billVal !== 0 && pplVal !== 0 && tipVal !== 0) {
@@ -32,26 +31,41 @@ export default function Home() {
     setPplInputVal('');
   }
 
-  useEffect(()=>{
-    pplInputVal && Number.parseInt(pplInputVal) <= 0 ? (document.querySelector('#pplSpan')?.classList.remove('invisible'), document.querySelector('#PplInput')?.classList.add('focus:border-red-500')) : (document.querySelector('#pplSpan')?.classList.add('invisible'), document.querySelector('#PplInput')?.classList.remove('focus:border-red-500'))   
-  },[pplInputVal])
+  useEffect(() => {
+    pplInputVal && Number.parseInt(pplInputVal) <= 0 ? (document.querySelector('#pplSpan')?.classList.remove('invisible'), document.querySelector('#PplInput')?.classList.add('focus:border-red-500')) : (document.querySelector('#pplSpan')?.classList.add('invisible'), document.querySelector('#PplInput')?.classList.remove('focus:border-red-500'))
+  }, [pplInputVal])
+
+  const handleButtonColor = (e:any) => {
+    let buttons = document.querySelectorAll("[name='tipButton']");
+    buttons.forEach((button) => {
+      button.classList.remove('bg-Scyan', 'text-VDcyan');
+      button.classList.add('bg-VDcyan', 'text-white')
+    })
+    if (e.target.classList.contains('bg-VDcyan')) {
+      e.target.classList.remove('bg-VDcyan', 'text-white')
+      e.target.classList.add('bg-Scyan', 'text-VDcyan')
+    }
+  }
 
   const buttonFunc = (label: string, value: number) => {
     return (
       <button
-        className="h-14 w-36 rounded-lg text-white bg-VDcyan text-2xl focus:bg-Scyan hover:bg-LGcyan hover:text-VDcyan focus:text-VDcyan"
-        onClick={() => setTipVal(value)}>
+        name="tipButton"
+        id={label}
+        className="h-14 w-32 rounded-lg text-white bg-VDcyan text-2xl focus:bg-Scyan hover:bg-LGcyan hover:text-VDcyan focus:text-VDcyan"
+        onClick={(e) => { setTipVal(value), handleButtonColor(e) }}
+      >
         {label}
       </button>
     )
   }
 
   return (
-    <div className="bg-LGcyan flex min-h-screen flex-col items-center md:p-24">
-      <div className="w-24 py-12 md:py-24">
+    <div className="bg-LGcyan flex min-h-screen flex-col items-center pt-12 md:p-24">
+      <div className="w-24 pb-12 md:pb-24">
         {logoComponent()}
       </div>
-      <div className="md:w-7/8 h-fit min-h-[500px] flex flex-wrap md:flex-nowrap  justify-center bg-white rounded-3xl">
+      <div className="md:w-4/5 h-fit min-h-[500px] flex flex-wrap md:flex-nowrap  justify-center bg-white rounded-3xl">
         <div className="col ml-12 mr-8 my-8 md:w-1/2 flex flex-col justify-around">
 
           <label className="block">
@@ -61,7 +75,7 @@ export default function Home() {
             <span className="absolute flex items-center my-5 mx-2">
               {dollarIconComponent()}
             </span>
-            <input id="BillInput" type="Bill" inputMode="numeric" name="Bill" className="text-end text-VDcyan text-2xl mt-1 px-3 py-2 bg-VLGcyan border-2 border-transparent shadow-sm placeholder-slate-400 focus:outline-none focus:border-Scyan block w-full rounded-md" placeholder="0" value={billInputVal} onChange={(e) => {setBillVal(Number.parseInt(e.target.value)), setBillInputVal(e.target.value)}} />
+            <input id="BillInput" type="Bill" inputMode="numeric" name="Bill" className="text-end text-VDcyan text-2xl mt-1 px-3 py-2 bg-VLGcyan border-2 border-transparent shadow-sm placeholder-slate-400 focus:outline-none focus:border-Scyan block w-full rounded-md" placeholder="0" value={billInputVal} onChange={(e) => { setBillVal(Number.parseInt(e.target.value)), setBillInputVal(e.target.value) }} />
           </label>
 
           <div className="block">
@@ -74,7 +88,7 @@ export default function Home() {
               {buttonFunc('15%', 15)}
               {buttonFunc('25%', 25)}
               {buttonFunc('50%', 50)}
-              <input className="h-14 w-36 rounded-lg text-DGcyan bg-VLGcyan text-2xl focus:ring-0 text-center" placeholder="Custom" onChange={(e) => setTipVal(Number.parseInt(e.target.value))}></input>
+              <input className="h-14 w-32 rounded-lg text-DGcyan bg-VLGcyan text-2xl focus:ring-0 text-center" placeholder="Custom" onChange={(e) => setTipVal(Number.parseInt(e.target.value))}></input>
             </div>
           </div>
 
@@ -85,11 +99,11 @@ export default function Home() {
             <span className="absolute flex items-center my-5 mx-2">
               {personIconComponent()}
             </span>
-            <input id="PplInput" type="People" name="People" className="text-end  text-VDcyan text-2xl border-2 border-transparent mt-1 px-3 py-2 bg-VLGcyan shadow-sm placeholder-slate-400 focus:outline-none focus:border-Scyan block w-full rounded-md" placeholder="0" min={1} aria-required="true" value={pplInputVal} onChange={(e) => {setPplVal(Number.parseInt(e.target.value)), setPplInputVal(e.target.value)}} />
+            <input id="PplInput" type="People" name="People" className="text-end  text-VDcyan text-2xl border-2 border-transparent mt-1 px-3 py-2 bg-VLGcyan shadow-sm placeholder-slate-400 focus:outline-none focus:border-Scyan block w-full rounded-md" placeholder="0" min={1} aria-required="true" value={pplInputVal} onChange={(e) => { setPplVal(Number.parseInt(e.target.value)), setPplInputVal(e.target.value) }} />
           </label>
 
         </div>
-        <div className="flex flex-col justify-between col w-full h-72 md:w-1/2 mr-8 md:mr-12 ml-8 my-8 p-8 bg-VDcyan rounded-3xl">
+        <div className="flex flex-col justify-between col w-full min-h-64 md:w-1/2 mr-8 md:mr-12 ml-8 my-8 p-8 bg-VDcyan rounded-3xl">
           <div className="row h-2/5 flex flex-col justify-between gap-y-4">
             <div className="row flex flex-row justify-between">
               <p className="text-white md:text-lg">Tip Amount<br /><span className="text-DGcyan text-md">/ person</span></p>
